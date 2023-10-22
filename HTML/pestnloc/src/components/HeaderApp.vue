02149c29ecdbb774c871568115d763cb289c918b
SuperSo6 <romain.gas@icloud.com>
2023-10-22 20:17:18 +0200

    Starting css of the header

--- HTML/pestnloc/src/components/HeaderApp.vue
+++ HTML/pestnloc/src/components/HeaderApp.vue
@@ -1,5 +1,39 @@
+
 <template>
-<h1>Hellow Wolrd !</h1>
+  <header>
+    <div class="divlogo">
+      <a>
+        <img  class="logo" src="../assets/logo.png"/> <!--Logo -->
+      </a>
+    </div>
+
+
+    <ul>
+      <li>
+        <a>Rent</a>
+      </li>
+
+      <li>
+        <a>Our Agencies</a>
+      </li>
+
+      <li>
+        <a> My Vehicules</a>
+      </li>
+
+      <li>
+        <div>
+<!--          <svg/>  Icon de Personne en svg -->
+          <a>Sing Up | Sign In</a>
+        </div>
+      </li>
+
+      <li>
+        <a>Langue</a>
+<!--        <svg/>  Drapeaux Pour modifier la Langue-->
+      </li>
+    </ul>
+  </header>
 </template>
 
 <script setup>
@@ -7,5 +41,7 @@
 </script>
 
 <style scoped>
+
+@import 'Css/Global.css';
 
 </style>
