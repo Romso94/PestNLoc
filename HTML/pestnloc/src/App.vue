88e744b7375c6b41d938f6fb9c4fab1ba1df324d
SuperSo6 <romain.gas@icloud.com>
2023-10-30 22:32:53 +0100

    Brand color for pestnloc

--- HTML/pestnloc/src/App.vue
+++ HTML/pestnloc/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <HeaderApp/>
+  <TestComponent/>
 
 </template>
 
 <script setup>
 
 import HeaderApp from "@/components/HeaderApp";
+import TestComponent from "@/components/TestComponent";
 
 
 </script>
