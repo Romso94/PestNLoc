bb850c7c60005e8c765c84885f7e83b6362a0901
Flyrik <flyrik8@gmail.com>
2023-10-31 16:29:24 +0100

    connection base de donné et js

--- HTML/pestnloc/server.js
+++ HTML/pestnloc/server.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2/promise');
 const conn = mysql.createConnection({
-    host:'localhost', user: 'root', database: 'carsdb', password: '', debug: false
+    host:'localhost', user: 'root', database: 'PesNloc', password: 'Florian1!', debug: false
 });
 conn.then(function(conn) {
-    conn.execute('SELECT * FROM users').then(function(result){ const [rows, fields]=result; console.log(rows); });
-    conn.execute('SELECT * FROM cars').then(function(result){ const [rows, fields]=result; console.log(rows); process.exit(); });
+    conn.execute('SELECT * FROM Contract').then(function(result){ const [rows, fields]=result; console.log(rows); });
+
 });
-return;
-*/
+
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -15,7 +14,6 @@ dotenv.config();
 const express = require('express');
 const app = express();
 app.set("view engine", "ejs");
-app.set("views", "views");
 
 app.listen(process.env.WEB_PORT, '0.0.0.0',
     function() { console.log("Listening on "+process.env.WEB_PORT); }
@@ -26,3 +24,4 @@ app.get('/', (request, response) => { // 'GET' as a HTTP VERB, not as a 'getter'
     response.send(`Hello, dear ${clientIp}. I am a nodejs website...`);
     response.end(); // optional
 });
+
