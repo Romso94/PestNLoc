9ea2badb0c0fe06a91787bb51afa34b4db11a660
SuperSo6 <romain.gas@icloud.com>
2023-11-03 01:07:44 +0100

    routage des composants

--- HTML/pestnloc/src/App.vue
+++ HTML/pestnloc/src/App.vue
@@ -1,42 +1,15 @@
 <template>
-  <HeaderApp @show-component="getComponentToShow" />
-  <LoginComponent v-if="listComponentToShow.loginComponent === true" />
+  <div class="app">
+
+    <router-view/>
+
+  </div>
+
 
 
 </template>
 
 <script setup>
-
-import HeaderApp from "@/components/HeaderApp";
-import LoginComponent from "@/components/LoginComponent.vue";
-import {ref} from "vue";
-
-
-const listComponentToShow = ref({
-  loginComponent: false,
-  home: false,
-});
-
-const getComponentToShow = (nameComponent) =>{
-  Object.keys(listComponentToShow.value).forEach((key) => {
-    listComponentToShow[key] = false;
-  });
-   switch (nameComponent){
-     case 'login' : {
-       listComponentToShow.value.loginComponent = true;
-       break;
-     }
-
-
-     default : {
-       break;
-     }
-
-   }
-}
-
-
-
 </script>
 
 <style>
