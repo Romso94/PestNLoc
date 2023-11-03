9528ce3b553bd2e2547de919ae2b3270a29e4847
SuperSo6 <romain.gas@icloud.com>
2023-11-03 09:57:09 +0100

    Launch server

--- HTML/pestnloc/server.js
+++ HTML/pestnloc/server.js
@@ -1,35 +1,34 @@
 const mysql = require('mysql2/promise');
-
-const conn = mysql.createConnection({
-    host:'localhost', user: 'root', database: 'PesNloc', password: 'Florian1!', debug: false
-});
-conn.then(function(conn) {
-    conn.execute('SELECT * FROM Contract').then(function(result){ const [rows, fields]=result; console.log(rows); });
-
-});
-
-
+const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
+//
+// const conn = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'PesNloc',
+//     password: 'Florian1!',
+//     debug: false
+// });
+// conn.then(function (conn) {
+//     conn.execute('SELECT * FROM Contract').then(function (result) {
+//         const [rows, fields] = result;
+//         console.log(rows);
+//     });
+// });
+
 dotenv.config();
 
-const express = require('express');
 const app = express();
 
+// Servez les fichiers statiques de l'application Vue.js depuis le répertoire "dist"
+app.use(express.static(path.join(__dirname, 'dist')));
 
-
-var path = require('path');
-var serveStatic = require('serve-static');
-
-app.use(serveStatic(__dirname + "/dist"));
-
-app.listen(process.env.WEB_PORT, '0.0.0.0',
-    function() { console.log("Listening on "+process.env.WEB_PORT); }
-);
-
-app.get('/', (request, response) => { // 'GET' as a HTTP VERB, not as a 'getter'!
-    let clientIp = request.ip;
-    response.send(`Hello, dear ${clientIp}. I am a nodejs website...`);
-    response.end(); // optional
+// Gérez toutes les autres routes en renvoyant l'index.html (SPA)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-
+app.listen(process.env.WEB_PORT || 3000, () => {
+    console.log("App launched on : \nhttp://localhost:" + (process.env.WEB_PORT || 3000));
+});
