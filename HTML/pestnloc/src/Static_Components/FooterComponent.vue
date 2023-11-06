d075b4289737debff5336e620e30c3bdc4e0f38e
Noé Roux <noe.roux@efrei.net>
2023-11-06 14:48:12 +0100

    creation footer

--- HTML/pestnloc/src/Static_Components/FooterComponent.vue
+++ HTML/pestnloc/src/Static_Components/FooterComponent.vue
@@ -1,11 +1,54 @@
 <template>
+
 <footer>
-  <div>
+
+
+  <div class = "container">
+    <div class = "row">
+
+      <div class = "footer-column">
+           <h4>About us</h4>
+            <ul>
+              <li><a href="#">Ex turba vero imae sortis et
+                paupertinae in tabernis aliqui pernoctant
+                vinariis, non nulli velariis umbraculorum
+                theatralium latent, quae Campanam imitatus
+                lasciviam Catulus in aedilitate sua suspendit
+                omnium primus;</a></li>
+            </ul>
+    </div>
+
+      <div class = "footer-column">
+        <h4>Our Localisation</h4>
+        <ul>
+          <li><a href="#">Budapest, 41 Baross utca</a></li>
+        </ul>
+      </div>
+
+
+
+      <div class = "footer-column">
+        <h4>Contact us</h4>
+        <ul>
+          <li><a href="#">+33617465312</a></li>
+          <li><a href="#">pestnloc@gmail.com</a></li>
+        </ul>
+      </div>
+
+      <div class = "footer-column">
+        <h4>Follow us</h4>
+        <div>
+          <div class ="Social networks">
+          <li><a href="#">On Snapchat</a></li>
+        </div>
+      </div>
+
+
   </div>
-  <div>
-    <ul> Test </ul>
   </div>
-</footer>
+  </div>
+
+  </footer>
 </template>
 
 <script setup>
@@ -13,5 +56,5 @@
 </script>
 
 <style scoped>
-
+@import '../components/Css/Footer.css';
 </style>
