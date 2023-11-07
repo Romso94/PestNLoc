25596f2a67105120fe472d9e42ee8a5d14331ac1
Flyrik <flyrik8@gmail.com>
2023-11-07 18:12:32 +0100

    etape 1 register

--- HTML/pestnloc/src/components/RegisterComponent.vue
+++ HTML/pestnloc/src/components/RegisterComponent.vue
@@ -4,28 +4,9 @@
   <div class="maindiv">
 
       <form class="formRegister">
-        <div class=""></div>
-        <div class="formRegisterInputs">
-          <div class="NameInput">
-            <label>Firs tName</label>
-            <input type="text" id="FirstName" required>
-            <label>Last Name</label>
-            <input type="text" id="LastName" required>
-          </div>
-          <div class="MailPasswordInput">
-            <label for="email">Email</label>
-            <input type="text" id="email" required>
-
-            <label for="password">Password</label>
-            <input type="password" id="password" required>
-            <label for="Confirmpassword">Confirm Password</label>
-            <input type="password" id="Confirmpassword" required>
-
-          </div>
-        </div>
 
 
-          <br><br>
+
 
           <input class="bouton" type="submit" value="Register" @click="validatePassword">
 
---
+++ HTML/pestnloc/src/components/RegisterEtape1.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="formRegisterInputs">
+    <div class="NameInput">
+      <label>Firs tName</label>
+      <input type="text" id="FirstName" required>
+      <label>Last Name</label>
+      <input type="text" id="LastName" required>
+    </div>
+    <div class="MailPasswordInput">
+      <label for="email">Email</label>
+      <input type="text" id="email" required>
+
+      <label for="password">Password</label>
+      <input type="password" id="password" required>
+      <label for="Confirmpassword">Confirm Password</label>
+      <input type="password" id="Confirmpassword" required>
+
+    </div>
+  </div>
+
+
+  <br><br>
+</template>
+
+<style scoped>
+
+@import 'Css/Register.css';
+
+</style>
