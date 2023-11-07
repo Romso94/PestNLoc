158b274b3510e6cec81836e9a13bfb7b5a46599a
SuperSo6 <romain.gas@icloud.com>
2023-11-07 18:15:27 +0100

    merged

--- HTML/pestnloc/src/components/RegisterComponent.vue
+++ HTML/pestnloc/src/components/RegisterComponent.vue
@@ -6,24 +6,7 @@
       <form class="formRegister">
 
 
-        <div class="formRegisterInputs">
-          <div class="NameInput">
-            <label>Firs tName</label>
-            <input type="text" id="FirstName" required>
-            <label>Last Name</label>
-            <input type="text" id="LastName" required>
-          </div>
-          <div class="MailPasswordInput">
-            <label for="email">Email</label>
-            <input type="text" id="email" required>
 
-            <label for="password">Password</label>
-            <input type="password" id="password" required>
-            <label for="Confirmpassword">Confirm Password</label>
-            <input type="password" id="Confirmpassword" required>
-
-          </div>
-        </div>
 
 
 
