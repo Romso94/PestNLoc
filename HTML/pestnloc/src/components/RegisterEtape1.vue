6045764632cdb8e41cf8b83217dfcb4b1f065453
Flyrik <flyrik8@gmail.com>
2023-11-16 13:48:11 +0100

    Modifie register

--- HTML/pestnloc/src/components/RegisterEtape1.vue
+++ HTML/pestnloc/src/components/RegisterEtape1.vue
@@ -5,14 +5,14 @@
 <template>
   <div class="formRegisterInputs">
     <div class="NameInput">
-      <label>Firs tName</label>
+      <label>First Name</label>
       <input type="text" id="FirstName" required>
       <label>Last Name</label>
       <input type="text" id="LastName" required>
     </div>
     <div class="MailPasswordInput">
       <label for="email">Email</label>
-      <input type="text" id="email" required>
+      <input type="email" id="email" required>
 
       <label for="password">Password</label>
       <input type="password" id="password" required>
--- HTML/pestnloc/src/components/RegisterEtape2.vue
+++ HTML/pestnloc/src/components/RegisterEtape2.vue
@@ -3,7 +3,14 @@
 </script>
 
 <template>
-  <div></div>
+
+  <div class="Agenumber">
+    <label for="Age">Age :</label>
+      <input type="number" id="age" name="age" min="18" required>
+
+      <label for="Phone_Number">Phone Number :</label>
+      <input type="tel" id="telephone" name="telephone" pattern="[0-9]{10}" placeholder="Format : 1234567890" required></div>
+  <div/>
 </template>
 
 <style scoped>
