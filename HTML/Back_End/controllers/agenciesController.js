55f2353c3c91b01fb0ffb6d41d9acf816a5eb7f5
Flyrik <flyrik8@gmail.com>
2023-11-21 14:56:01 +0100

    agency model and controller advanc

--- HTML/Back_End/controllers/agenciesController.js
+++ HTML/Back_End/controllers/agenciesController.js
@@ -5,4 +5,49 @@
 //     - `updateRental`: Met à jour une location de voiture existante.
 //     - `deleteRental`: Supprime une location de voiture.
 
-const Agencies = require('../models/clientsModel.js');
+const Agencies = require('../models/agenciesModel.js');
+
+const getRentals = async (req, res) => {
+    try {
+      const agencies = await Contract.getAllAgencies();
+      res.json(agencies);
+      console.log(agencies);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+
+
+const getRentalById = async (req,res) =>{
+  const {id} = req.params;
+  try {
+  const agency= await Contract.getAgencyById(id);
+  res.json(agency);
+  console.log(agency);
+} catch (error) {
+  console.error(error);
+  res.status(500).json({ error: 'Internal Server Error' });
+}
+}
+
+
+
+const createRental = async (req,res) =>{}
+
+const updateRental = async (req,res) =>{}
+
+const deleteRental = async (req,res) =>{
+  const {id} = req.params;
+  try {
+  const agencies = await Agencies.deleteAgency(id);
+    res.json(Agencies);
+  console.log("Delete Successfull");
+} catch (error) {
+  console.error(error);
+  res.status(500).json({ error: 'Internal Server Error' });
+}
+
+}
+
+module.exports = {getRentals, getRentalById, createRental, updateRental, deleteRental};
--- HTML/Back_End/models/agenciesModel.js
+++ HTML/Back_End/models/agenciesModel.js
@@ -3,3 +3,53 @@
 // - `createAgency`: Crée une nouvelle agence.
 // - `updateAgency`: Met à jour une agence existante.
 // - `deleteAgency`: Supprime une agence.
+
+const getAllAgencies = async () => {
+    try{
+
+        const query = "SELECT * FROM AGENCY";
+        const result = await execute(query);
+
+        return result
+        
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
+}
+
+const getAgencyById = async (agencyID) => {
+    try{
+
+        const query = "SELECT * FROM AGENCY Where Id_Agency= ? ";
+        const result = await execute(query,[agencyID]);
+
+        return result
+        
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
+}
+
+const createAgency = async (agencyData) => {}
+
+const updateAgency = async (agencyUpdateData) => {}
+
+const deleteAgency = async (agencyID) => {
+    try{
+
+        const query = "DELETE FROM AGENCY Where Id_Agency=?";
+        const result = await execute(query,[agencyID]);
+
+        return result
+        
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
+
+}
+
+
+module.exports = { getAllAgencies, getAgencyById, createAgency, updateAgency, deleteAgency};
