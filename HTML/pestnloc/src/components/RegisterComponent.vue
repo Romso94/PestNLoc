9a5683bdf85b566d65d9f5181bf169f2e87ad21c
Flyrik <flyrik8@gmail.com>
2023-11-23 09:50:51 +0100

    Previous modification

--- HTML/pestnloc/src/components/RegisterComponent.vue
+++ HTML/pestnloc/src/components/RegisterComponent.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="maindiv">
-
     <div class="formRegister">
-      <component :is="currentStepComponent " />
+      <component :is="currentStepComponent" />
 
-      <button class="bouton" @click="showPreviousStep"  :disabled="currentStep.value === 1" >Previous</button>
+      <button class="bouton" @click="showPreviousStep" :disabled="currentStep.value === 1">Previous</button>
 
-      <button class="bouton" @click="showNextStep"  >Next</button>
+      <button class="bouton" @click="showNextStep">Next</button>
       <a href="/login" onclick="">You already have an account? Click Here to Sign In!</a>
     </div>
-
   </div>
 </template>
 
 <script setup>
-import { ref,computed } from 'vue';
+import { ref, computed } from 'vue';
+import RegisterEtape1 from "@/components/RegisterEtape1.vue";
+import RegisterEtape2 from "@/components/RegisterEtape2.vue";
+import RegisterEtape3 from "@/components/RegisterEtape3.vue";
 
 const currentStep = ref(1);
 
@@ -26,46 +27,21 @@ const currentStepComponent = computed(() => {
   } else if (currentStep.value === 3) {
     return RegisterEtape3;
   } else {
-  return RegisterEtape1;
-}
+    return RegisterEtape1;
+  }
 });
-
 
 const showNextStep = () => {
   currentStep.value += 1;
   if (currentStep.value > 3) currentStep.value = 1;
 };
 
-
 const showPreviousStep = () => {
-  currentStep.value = currentStep.value === 1 ? 3 : currentStep.value - 1;
+  // Adjust the logic to ensure it stays on the first step
+  currentStep.value = currentStep.value === 1 ? 1 : currentStep.value - 1;
 };
-
-
-import RegisterEtape1 from "@/components/RegisterEtape1.vue";
-import RegisterEtape2 from "@/components/RegisterEtape2.vue";
-import RegisterEtape3 from "@/components/RegisterEtape3.vue";
-
-
-//const  validatePassword = () => {
-//  var password = document.getElementById("password").value;
-//  var confirmPassword = document.getElementById("Confirmpassword").value;
-//  var errorElement = document.getElementById("passwordMatchError");
-
-//  if (password !== confirmPassword) {
-//    errorElement.textContent = "Passwords do not match. Please try again.";
-//    return false;
-//  } else {
-//    errorElement.textContent = "";
-//    return true;
-//  }
-//}
-
-
 </script>
-
 
 <style scoped>
 @import 'Css/Register.css';
-
 </style>
