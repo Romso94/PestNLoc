30d53372beaf5153c50ce3130e35411a2c9311c3
Flyrik <flyrik8@gmail.com>
2023-11-26 14:55:15 +0100

    contrat

--- HTML/pestnloc/src/components/ContractsComponent.vue
+++ HTML/pestnloc/src/components/ContractsComponent.vue
@@ -1,29 +1,7 @@
-
-
 <template>
   <div class="topMain">
-    <table>
-      <thead>
-      <tr>
-        <th>Availability</th>
-        <th>Start Date</th>
-        <th>Price</th>
-        <th>End Date</th>
-        <th>Client ID</th>
-        <th>Registration ID</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="contract in contractData" :key="contract.Id_Registration">
-        <td>{{ contract.Contract_Availability }}</td>
-        <td>{{ contract.Start_Date }}</td>
-        <td>{{ contract.Price }}</td>
-        <td>{{ contract.End_Date }}</td>
-        <td>{{ contract.Id_Client }}</td>
-        <td>{{ contract.Id_Registration }}</td>
-      </tr>
-      </tbody>
-    </table>
+    <v-data-table :items="contractData"></v-data-table>
+
   </div>
 
 </template>
@@ -31,13 +9,29 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 
-const contractData = ref([]);
+const contractData = ref([
+  {
+    "Contract_Availability": 365,
+    "Start_Date": "2023-01-15",
+    "Price": "150.0",
+    "End_Date": "2023-02-15",
+    "Id_Client": 1,
+    "Id_Registration": "ABC123"
+  },
+  {
+    "Contract_Availability": 365,
+    "Start_Date": "2023-02-01",
+    "Price": "200.0",
+    "End_Date": "2023-03-01",
+    "Id_Client": 2,
+    "Id_Registration": "DEF456"
+  }
+]);
 
 onMounted(async () => {
-
-  const jsonData = await import('@/Json/Contract.json');
-  contractData.value = jsonData;
+  console.log(contractData);
 });
+
 
 </script>
 
@@ -55,8 +49,5 @@ p{
   width: 100%;
   margin-top: 50px;
 }
-
-
-
 
 </style>
