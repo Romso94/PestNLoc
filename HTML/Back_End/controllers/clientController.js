1a215213ecc5035114a3f3f445ce81a3b9b5e370
SuperSo6 <romain.gas@icloud.com>
2023-11-26 01:33:48 +0100

    nouveau controller pour gerer completement le model client

---
+++ HTML/Back_End/controllers/clientController.js
@@ -0,0 +1,45 @@
+const Client = require("../models/clientsModel");
+
+async function getAllClient(req, res) {
+
+    try {
+        const clients = await Client.getAllClient();
+        res.json(clients);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erreur interne du serveur' });
+    }
+}
+
+async function getClientById(req, res) {
+    const { idClient } = req.params;
+
+    try {
+        const clients = await Client.getClientById(idClient);
+        res.json(clients);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erreur interne du serveur' });
+    }
+}
+
+async function deleteClient(req, res) {
+    const { idClient } = req.params;
+
+    try {
+        const clients = await Client.deleteClient(idClient);
+        res.json(clients);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erreur interne du serveur' });
+    }
+}
+
+module.exports = {
+    getAllClient,
+    getClientById,
+    deleteClient
+}
