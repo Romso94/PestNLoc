9cd5f548c09919bf0b2ac36233bd67594750c9cb
SuperSo6 <romain.gas@icloud.com>
2023-11-30 18:05:32 +0100

    login form

--- HTML/pestnloc/src/components/LoginComponent.vue
+++ HTML/pestnloc/src/components/LoginComponent.vue
@@ -1,33 +1,49 @@
 <template>
   <div class="maindiv">
-    <div class="formulaire">
-      <form>
-        <div class="formLogin">
-          <tr>
-            <td>
-              <label for="email">Email</label>
-              <input type="text" id="email" required>
-              <br><br>
-              <label for="password">Password</label>
-              <input type="password" id="password" required>
-              <br><br>
-              <input class="bouton" type="submit" value="Login">
-            </td>
-            <td>
-              <a href="/register"  onclick="">No account ? Click Here to Sign up !</a>
-            </td>
-          </tr>
+    <div class="divFormulaire">
+      <h1 class="titleLogin">Sign in</h1>
+      <form class="formulaire">
+        <input class="inputEmail" type="text" placeholder="Email" />
+        <div class="password-container">
+          <input
+              class="inputPassword"
+              :type="showPassword ? 'text' : 'password'"
+              placeholder="Password"
+              v-model="password"
+          />
+          <button type="button" class="togglePassword" @click="togglePasswordVisibility">
+            <i v-if="showPassword" class="material-icons">visibility</i>
+            <i v-else class="material-icons">visibility_off</i>
+          </button>
         </div>
+        <button class="submitLogin" type="submit">Login</button>
       </form>
+      <a class="linkSignup" href="/register">Sign up</a>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const password = ref('');
+const showPassword = ref(false);
+
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value;
+};
+
+const submitForm = () => {
+  // Ajoutez ici la logique pour soumettre le formulaire si nécessaire
+  // Par exemple, vous pouvez ajouter une requête AJAX pour gérer la connexion
+};
 </script>
 
 <style scoped>
 @import 'Css/Login.css';
+
+
+
+
 
 </style>
