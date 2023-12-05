acd6f90d981a938d6140b96ab043e0f088cb68a0
Noé Roux <noe.roux@efrei.net>
2023-12-05 19:21:53 +0100

    I have modified the contract page

--- HTML/pestnloc/src/components/ContractsComponent.vue
+++ HTML/pestnloc/src/components/ContractsComponent.vue
@@ -1,36 +1,19 @@
 <template>
   <div class="topMain">
     <div id="app">
-      <div v-if="contrat">
-        <!-- Affiche le contrat existant s'il y en a un -->
-        <h2>Contrat actuel :</h2>
-        <p>{{ contrat }}</p>
-      </div>
-      <div v-else>
-        <!-- Affiche le bouton pour afficher le formulaire -->
-        <button @click="toggleForm">Ajouter un contrat</button>
-
-<<<<<<< HEAD
-        <!-- Affiche le formulaire de contrat s'il n'y a pas de contrat existant -->
-        <ContractForm v-if="showForm" @contratAjoute="contratAjoute" />
-      </div>
-    </div>
-  </div>
-</template>
-
-<script setup>
-import { ref } from 'vue';
-import ContractForm from "./ContractFormComponent.vue";
-
-const contrat = ref(null);
-const showForm = ref(false);
-=======
+      <div>
   <div id="app">
     <div class ="AddNameLogout">
-        <button @click="ajouterContrat" class="ml-auto">Ajouter un contrat</button>
-        <div class="Name">Welcome Name Surname</div>
-      <button @click="logout" color="primary" class="ml-auto">Logout</button>
+
+        <div class="NameandSurname">Welcome {{Name}} {{ Surname }}</div>
+      <button @click="logout" color="primary" class="Buttoncontract">Logout</button>
     </div>
+    <div class ="AddNameLogout">
+      <div>You don't have any contracts</div>
+    </div>
+    <div class ="AddNameLogout">
+    <button @click="ajouterContrat" class="Buttoncontract Buttoncontract2">Ajouter un contrat</button>
+      </div>
     <h2>Contrat actuel :</h2>
   </div>
   </div>
@@ -39,19 +22,20 @@ const showForm = ref(false);
       <!-- Affiche le contrat existant s'il y en a un -->
       <p>{{ contrat }}</p>
     </div>
-
-
+    </div>
+  </div>
 </template>
 
 <script setup>
 import jsonData from '@/Json/car.json';
 const items = jsonData;
+const Name = "Florian";
+const Surname = "Poscente"
 
 const logout = () => {
   document.cookie = 'jwt=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
   window.location.href = '/login';
 };
->>>>>>> f9f980b5ed716de1493c4ae1b0701c826a92b108
 
 const contratAjoute = (nouveauContrat) => {
   contrat.value = nouveauContrat;
@@ -64,22 +48,8 @@ const toggleForm = () => {
 </script>
 
 <style scoped>
-<<<<<<< HEAD
-p {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
 
-.topMain {
-  background: var(--pestnlocColor);
-  height: 250px;
-  width: 100%;
-  margin-top: 50px;
-}
-</style>
-=======
 @import  "Css/Contract.css";
 
 </style>
->>>>>>> f9f980b5ed716de1493c4ae1b0701c826a92b108
+
