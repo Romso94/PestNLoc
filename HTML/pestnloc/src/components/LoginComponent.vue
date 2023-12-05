2a34a25e94fa1ea68503e4b5946069c98c9d2113
Flyrik <flyrik8@gmail.com>
2023-12-05 14:40:34 +0100

    modif

--- HTML/pestnloc/src/components/LoginComponent.vue
+++ HTML/pestnloc/src/components/LoginComponent.vue
@@ -2,8 +2,8 @@
   <div class="maindiv">
     <div class="divFormulaire">
       <h1 class="titleLogin">Sign in</h1>
-      <form class="formulaire">
-        <input class="inputEmail" type="text" placeholder="Email" />
+      <form class="formulaire" @submit.prevent="submitLoginForm">
+        <input class="inputEmail" type="text" placeholder="Email" v-model="email" />
         <div class="password-container">
           <input
               class="inputPassword"
@@ -26,6 +26,7 @@
 <script setup>
 import { ref } from 'vue';
 
+const email = ref('');
 const password = ref('');
 const showPassword = ref(false);
 
@@ -33,17 +34,34 @@ const togglePasswordVisibility = () => {
   showPassword.value = !showPassword.value;
 };
 
-const submitForm = () => {
-  // Ajoutez ici la logique pour soumettre le formulaire si nécessaire
-  // Par exemple, vous pouvez ajouter une requête AJAX pour gérer la connexion
+const submitLoginForm = async () => {
+  try {
+    const response = await fetch('/pestnloc/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        Email: email.value,
+        Password: password.value,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP : ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+
+    // Traitez le résultat comme nécessaire (par exemple, redirigez l'utilisateur, stockez le token, etc.)
+  } catch (error) {
+    console.error('Erreur lors de la requête :', error);
+    // Traitez l'erreur de manière appropriée (affichez un message d'erreur, par exemple)
+  }
 };
 </script>
 
 <style scoped>
 @import 'Css/Login.css';
-
-
-
-
-
 </style>
