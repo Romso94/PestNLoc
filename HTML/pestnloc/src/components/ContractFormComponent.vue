9683d5e42a56e69577f719014b6d4febf3f02bce
Flyrik <flyrik8@gmail.com>
2023-12-05 18:23:21 +0100

    con

---
+++ HTML/pestnloc/src/components/ContractFormComponent.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+sdds
+</template>
+
+<style scoped>
+
+</style>
--- HTML/pestnloc/src/components/ContractsComponent.vue
+++ HTML/pestnloc/src/components/ContractsComponent.vue
@@ -1,41 +1,50 @@
-
 <template>
   <div class="topMain">
+    <div id="app">
+      <div v-if="contrat">
+        <!-- Affiche le contrat existant s'il y en a un -->
+        <h2>Contrat actuel :</h2>
+        <p>{{ contrat }}</p>
+      </div>
+      <div v-else>
+        <!-- Affiche le bouton pour afficher le formulaire -->
+        <button @click="toggleForm">Ajouter un contrat</button>
 
-  <div id="app">
-    <div v-if="contrat">
-      <!-- Affiche le contrat existant s'il y en a un -->
-      <h2>Contrat actuel :</h2>
-      <p>{{ contrat }}</p>
-    </div>
-    <div v-else>
-      <!-- Affiche un bouton pour ajouter un contrat si aucun contrat n'existe -->
-      <button @click="ajouterContrat">Ajouter un contrat</button>
+        <!-- Affiche le formulaire de contrat s'il n'y a pas de contrat existant -->
+        <ContractForm v-if="showForm" @contratAjoute="contratAjoute" />
+      </div>
     </div>
   </div>
-  </div>
-
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import ContractForm from "./ContractFormComponent.vue";
 
+const contrat = ref(null);
+const showForm = ref(false);
 
+const contratAjoute = (nouveauContrat) => {
+  contrat.value = nouveauContrat;
+  showForm.value = false; // Masquer le formulaire après l'ajout du contrat
+};
 
+const toggleForm = () => {
+  showForm.value = !showForm.value;
+};
 </script>
 
 <style scoped>
-
-  p{
+p {
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
-  .topMain{
+.topMain {
   background: var(--pestnlocColor);
   height: 250px;
   width: 100%;
   margin-top: 50px;
 }
-
 </style>
