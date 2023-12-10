2ece199fb941b3efe10bfabd3ab3c007a41e0619
Noé Roux <noe.roux@efrei.net>
2023-12-10 17:04:20 +0100

    I have modified the contract page

--- HTML/pestnloc/src/components/ContractsComponent.vue
+++ HTML/pestnloc/src/components/ContractsComponent.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="topMain">
-    <div id="app">
+    <div class="Contract_component">
 
-      <div>
 
-        <div id="app">
           <div class ="AddNameLogout">
 
             <div class="NameandSurname">Welcome {{Name}} {{ Surname }}</div>
@@ -13,18 +11,21 @@
           <div class ="AddNameLogout">
             <div>You don't have any contracts</div>
           </div>
-          <div class ="AddNameLogout">
-            <button @click="ajouterContrat" class="Buttoncontract Buttoncontract2">Ajouter un contrat</button>
-          </div>
+
+          <div class="White_contract">
           <h2>Contrat actuel :</h2>
-        </div>
-      </div>
+          <div>Id Client : {{Id_client}}</div>
+          <div>Id Contract : {{Id_contract}}</div>
+          <div>Starting Date : {{Start_Date_Year}}-{{Start_Date_Month}}-{{Start_Date_Day}}</div>
+          <div>Ending Date : {{End_Date_Year}}-{{End_Date_Month}}-{{End_Date_Day}}</div>
+          <div>Price : {{Price}}</div>
+          <div>License Plate : {{License_Plate}}</div>
+          </div>
+
       <div>
-        <!-- Affiche le bouton pour afficher le formulaire -->
-        <button @click="toggleForm">Ajouter un contrat</button>
-
-
-        <!-- Affiche le formulaire de contrat s'il n'y a pas de contrat existant -->
+        <div class ="AddNameLogout">
+          <button @click="ajouterContrat" class="Buttoncontract Buttoncontract2">Ajouter un contrat</button>
+        </div>
         <ContractForm v-if="showForm" @contratAjoute="contratAjoute" />
       </div>
     </div>
@@ -51,8 +52,18 @@ export default {
        contrat : shallowRef(null),
        showForm : shallowRef(false),
        items : jsonData,
-       Name : "Florian",
-       Surname : "Poscente",
+       Name : shallowRef(""),
+       Surname : shallowRef(""),
+      Id_client : shallowRef(""),
+      Id_contract : shallowRef(""),
+      Start_Date_Year : shallowRef(""),
+      Start_Date_Month : shallowRef(""),
+      Start_Date_Day : shallowRef(""),
+      End_Date_Year : shallowRef(""),
+      End_Date_Month : shallowRef(""),
+      End_Date_Day : shallowRef(""),
+      License_Plate : shallowRef(""),
+      Price : shallowRef(""),
       responseData : null
     }
   },
@@ -90,7 +101,40 @@ export default {
         if (response.ok) {
 
           const responseData = await response.json();
-          console.log(responseData[0]);
+          console.log(responseData[0].Id_Contract);
+          this.Id_client = responseData[0].Id_Client;
+          this.Id_contract = responseData[0].Id_contract;
+          this.Start_Date_Year = responseData[0].Start_Date.slice(0,4);
+          this.Start_Date_Month = responseData[0].Start_Date.slice(5,7);
+          this.Start_Date_Day = responseData[0].Start_Date.slice(8,10);
+          this.End_Date_Year = responseData[0].End_Date.slice(0,4);
+          this.End_Date_Month = responseData[0].End_Date.slice(5,7);
+          this.End_Date_Day = responseData[0].End_Date.slice(8,10);
+          this.License_Plate = responseData[0].License_Plate;
+          this.Price = responseData[0].Price;
+
+
+          try {
+            const response = await fetch(`http://localhost:9000/pestnloc/clients/${this.Id_client}`, {
+              method: 'GET',
+              headers:{
+                'Authorization': `Bearer ${sendCookie}`
+              }});
+
+            if (response.ok){
+              const infoclient = await response.json();
+              console.log(infoclient)
+              this.Name = infoclient[0].Name;
+              this.Surname = infoclient[0].LastName;
+
+            }
+          }
+
+          catch (error){
+            console.log('Erreur lors de la récupération des infos clients')
+          }
+
+
         } else {
           console.error('La requête a échoué avec le statut :', response.status);
         }
